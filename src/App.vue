ae444a8779b66b6e5eb9ff03d11aab5f434c74e5
Maxwell Muhanda <ajaybullec@gmail.com>
2023-03-01 16:19:46 +0300

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import Magnify from 'vue-material-design-icons/Magnify.vue'
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue'
+import Television from 'vue-material-design-icons/Television.vue'
+import MovieOutline from 'vue-material-design-icons/MovieOutline.vue'
+import Plus from 'vue-material-design-icons/Plus.vue'
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
+import VideoCarousel from '@/components/VideoCarousel.vue'
+import MovieDetails from '@/components/MovieDetails.vue'
+</script>
+
+<template>
+	<div class="text-red-900">Text HERE</div>
+</template>
